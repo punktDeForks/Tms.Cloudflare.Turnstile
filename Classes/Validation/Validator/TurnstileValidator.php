1ba52aa5427018df868f7964e47b985b1dca527a
Joachim Beck <beck@tms-institut.de>
2023-02-20 13:50:25 +0100

    first commit

---
+++ Classes/Validation/Validator/TurnstileValidator.php
@@ -0,0 +1,71 @@
+<?php
+namespace Tms\Cloudflare\Turnstile\Validation\Validator;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Http\Client\Browser;
+use Neos\Flow\Http\Client\CurlEngine;
+use Neos\Flow\Log\Utility\LogEnvironment;
+use Neos\Flow\Validation\Validator\AbstractValidator;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Turnstile validator
+ */
+class TurnstileValidator extends AbstractValidator
+{
+    /**
+     * @Flow\Inject
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var boolean
+     */
+    protected $acceptsEmptyValues = false;
+
+    /**
+     * @var array
+     */
+    protected $supportedOptions = [
+        'endpoint' => ['', 'Turnstile API endpoint', 'string', true],
+        'secretKey' => ['', 'Secret key of your Turnstile site', 'string', true]
+    ];
+
+    /**
+     * @param mixed $value The value that should be validated
+     *
+     * @return void
+     * @throws \Neos\Flow\Validation\Exception\InvalidValidationOptionsException
+     */
+    protected function isValid($value)
+    {
+        if (!is_string($value) || empty($value)) {
+            $this->addError('We could not identify you as a human. Please try again.', 1676890456);
+            return;
+        }
+
+        $requestEngine = new CurlEngine();
+        $requestEngine->setOption(CURLOPT_TIMEOUT, 60);
+
+        $browser = new Browser();
+        $browser->setRequestEngine($requestEngine);
+
+        $arguments['secret'] = $this->getOptions()['secretKey'];
+        $arguments['response'] = $value;
+
+        try {
+            $response = $browser->request($this->getOptions()['endpoint'], 'POST', [], [], [], http_build_query($arguments));
+            $responseContent = json_decode($response->getBody()->getContents(), true);
+            $this->logger->debug(json_encode($responseContent), LogEnvironment::fromMethodName(__METHOD__));
+
+            if (!$responseContent['success']) {
+                $this->logger->error(json_encode($responseContent), LogEnvironment::fromMethodName(__METHOD__));
+                $this->addError('We could not identify you as a human. Please try again.', 1676890456);
+            }
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage(), LogEnvironment::fromMethodName(__METHOD__));
+            $this->addError('We could not identify you as a human. Please try again.', 1676890456);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,49 @@
+# Tms.Cloudflare.Turnstile
+
+This package provides a form element for the CAPTCHA alternative [Turnstile by Cloudflare](https://developers.cloudflare.com/turnstile/) and can be used on your Neos CMS site with [Neos.Form](https://github.com/neos/form) & [Neos.Form.Builder](https://github.com/neos/form-builder).
+
+
+> Turnstile is Cloudflare’s smart CAPTCHA alternative. It can be embedded into any website without sending traffic through Cloudflare and works without showing visitors a CAPTCHA.
+
+Cloudflare Turnstile documentation: https://developers.cloudflare.com/turnstile/
+
+## Install
+
+```bash
+composer require tms/cloudflare-turnstile
+```
+
+## Usage
+
+1.) [Create a free Cloudflare account](https://dash.cloudflare.com/sign-up) or [log in](https://dash.cloudflare.com/login) to your existing one
+
+2.) Go to **Turnstile**
+
+3.) Select **Add a site** and fill out the form
+
+4.) Copy the `site key` and `secret key` and add the following environment variables
+
+```yaml
+# Configuration/Settings.yaml
+Tms:
+  Cloudflare:
+    Turnstile:
+      siteKey: '%env:CLOUDFLARE_TURNSTILE_SITE_KEY%'
+      secretKey: '%env:CLOUDFLARE_TURNSTILE_SECRET_KEY%'
+```
+
+**Note:** In `Development` context we automatically set a [test key pair](https://developers.cloudflare.com/turnstile/frequently-asked-questions/#are-there-sitekeys-and-secret-keys-that-can-be-used-for-testing)
+
+5.) Add the Turnstile form element to your form configuration or use the Turnstile content element in your node-based forms
+
+```
+prototype(Vendor.PackageName:MyForm) < prototype(Neos.Form.Builder:Form) {
+    firstPage.elements {
+        turnstile = Tms.Cloudflare.Turnstile:Turnstile.Definition
+    }
+}
+```
+
+## Acknowledgments
+
+Development sponsored by [tms.development - Online Marketing and Neos CMS Agency](https://www.tms-development.de/)
